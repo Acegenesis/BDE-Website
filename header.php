8a9fb1b8957925625089c538b8b87bd28217fb90
Kronky <66308616+Acegenesis@users.noreply.github.com>
2024-03-30 23:09:29 +0100

    Modification header

--- header.php
+++ header.php
@@ -13,10 +13,10 @@
                 <span class="hamburger-inner"></span>
             </span>
         </button>
-        <div class="logos">
+        <a href="<?php echo home_url();?>" class="logos">
             <img id="logo" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/logo.png" alt="Logo du site">
             <img id="logo_alt" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/logo_alt.png" alt="Logo du site">
-        </div>
+        </a>
     </header>
     <nav id="nav">
         <div class="presentation">
@@ -36,14 +36,14 @@
         </ul>
     </nav>
     <script>
-    var hamburger = document.querySelector(".hamburger");
-    var nav = document.querySelector("#nav");
-    var header = document.querySelector("#header");
-    hamburger.addEventListener("click", function() {
-        hamburger.classList.toggle("is-active");
-        nav.classList.toggle("is-active");
-        header.classList.toggle("is-active");
-    });
+        var hamburger = document.querySelector(".hamburger");
+        var nav = document.querySelector("#nav");
+        var header = document.querySelector("#header");
+        hamburger.addEventListener("click", function() {
+            hamburger.classList.toggle("is-active");
+            nav.classList.toggle("is-active");
+            header.classList.toggle("is-active");
+        });
     </script>
     <main>
     
