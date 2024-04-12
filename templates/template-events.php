265d31e48b01e908e3b3ce91054948d5f5a5ef6c
Kronky <66308616+Acegenesis@users.noreply.github.com>
2024-04-12 02:43:44 +0200

    Ajout de la page avec tous les événements listé + système de pages

---
+++ templates/template-events.php
@@ -0,0 +1,80 @@
+<?php
+/*
+Template Name: Liste des événements
+*/
+
+get_header();
+
+// Récupérer les événements avec WP_Query
+$event_per_page = 4; // Nombre d'événements par page
+$paged = ( get_query_var( 'paged' ) ) ? get_query_var( 'paged' ) : 1;
+$args = array(
+    'post_type'      => 'events',
+    'posts_per_page' => $event_per_page, // Nombre d'événements par page
+    'paged'          => $paged, // Page actuelle
+    'order'          => 'DESC', // Ordre décroissant (du plus récent au plus ancien)
+);
+
+$query = new WP_Query( $args );
+
+if ( $query->have_posts() ) :
+    ?>
+    <h1>Liste des événements</h1>
+    <div class="container">
+        <?php
+        while ( $query->have_posts() ) : $query->the_post();
+            $event_url = get_permalink(); // URL de l'événement
+            $event_id = get_the_ID();
+            $slider_image = get_post_meta( $event_id, 'slider_image', true );
+            ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <a href="<?php echo $event_url; ?>">
+                    <h2 class="entry-title"><?php the_title(); ?></h2>
+                    <img src="<?php echo $slider_image; ?>" alt="<?php the_title(); ?>" />
+                    <div class="more">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+                            <!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
+                            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+                        </svg>
+                    </div>
+                </a>
+            </article>
+            <?php
+        endwhile;
+        if( $paged == $query->max_num_pages && $query->have_posts() % 2 != 0 ) :
+            ?>
+            <article>
+            </article>
+            <?php
+            endif;
+        ?>
+    </div>
+    <?php
+    // Affichage de la pagination
+    $big = 999999999;
+    echo paginate_links( array(
+        'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+        'format'    => '?paged=%#%',
+        'current'   => max( 1, $paged ),
+        'total'     => $query->max_num_pages,
+        'prev_text' => 'Précédent', // Texte pour le lien précédent
+        'next_text' => 'Suivant', // Texte pour le lien suivant
+        'mid_size'  => 1, // Nombre de liens à afficher de chaque côté de la page actuelle
+        'type'      => 'list', // Type de format de pagination (ici une liste de balises <li>)
+        'end_size'  => 1, // Nombre de liens à afficher aux extrémités
+        'before_page_number' => '<span class="page-number">', // Balise avant le numéro de page
+        'after_page_number' => '</span>', // Balise après le numéro de page
+    ) );
+    
+    ?>
+    <?php
+    wp_reset_postdata(); // Réinitialiser la requête
+else :
+    // Aucun événement trouvé
+    ?>
+    <p>Aucun événement trouvé.</p>
+    <?php
+endif;
+
+get_footer();
+?>
